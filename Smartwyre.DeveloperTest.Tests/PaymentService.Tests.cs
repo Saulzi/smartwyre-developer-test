baa162b5a4bd089e63a2301f902c6f084ac0e9f3
Tom Salisbury <mrsaulzi@gmail.com>
2022-05-21 00:47:22 +0100

    Added FakeItEasy and FluentAssertions to testing to allow creating test of the happy path of the constructor

--- Smartwyre.DeveloperTest.Tests/PaymentService.Tests.cs
+++ Smartwyre.DeveloperTest.Tests/PaymentService.Tests.cs
@@ -1,6 +1,9 @@
 using System;
+using FakeItEasy;
+using Smartwyre.DeveloperTest.Data;
 using Smartwyre.DeveloperTest.Services;
 using Xunit;
+using FluentAssertions;
 
 namespace Smartwyre.DeveloperTest.Tests
 {
@@ -15,9 +18,20 @@ namespace Smartwyre.DeveloperTest.Tests
             Action act = () => new PaymentService(null);
 
             // Assert
-            Assert.Throws<ArgumentNullException>(act);
+            act.Should().Throw<ArgumentNullException>().WithParameterName("accountDataStore");
         }
 
-        
+        [Fact]
+        public void PaymentService_ConstructorPassedDataSource_DoesntThrow()
+        {
+            // Arrange
+            var fake = A.Fake<IAccountDataStore>();
+
+            // Act
+            Action act = () => new PaymentService(fake);
+
+            // Assert
+            act.Should().NotThrow();
+        }        
     }
 }
